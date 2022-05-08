257ae56f772656c791286829d7393f5c820a1c01
ponta <mponta2002@gmail.com>
2022-05-08 13:05:42 +0900

    どっか―テスト

---
+++ docker-test/mysql/docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,56 @@
+DROP SCHEMA IF EXISTS shukatsu;
+
+CREATE SCHEMA shukatsu;
+
+USE shukatsu;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+INSERT INTO
+  users
+SET
+  email = 'test@posse-ap.com',
+  password = sha1('password');
+
+DROP TABLE IF EXISTS events;
+
+CREATE TABLE events (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+INSERT INTO
+  events
+SET
+  title = 'イベント1';
+
+INSERT INTO
+  events
+SET
+  title = 'イベント2';
+
+
+
+DROP TABLE IF EXISTS company;
+
+CREATE TABLE company (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  names VARCHAR(255) NOT NULL,
+  link VARCHAR(255) NOT NULL,
+);
+
+INSERT INTO 
+company (names , link)
+VALUES
+('マイナビ','mainabisyuukatu.com'),
+('doda','dodakatukatu.com');
